e87ac3e1f155acbd0089736c3e763deab1bc0778
Jesus24-Dev <siritjesus24@gmail.com>
2025-08-05 20:58:11 -0400

    add midlewares and sesiones

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const {index} = require('../controllers/index.controller');
+const sessionCheck = require('../middlewares/sessionCheck')
 
-router.get('/', index);
-
+router.get('/', sessionCheck, index);
+// router.get('/', index);
 module.exports = router;
