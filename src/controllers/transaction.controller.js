5f35758e61e6fdbe920a56c08380b5b719ff59fd
Javier_Barrios <javierbarrios89@gmail.com>
2025-08-19 22:38:35 -0400

    Is possible to add transactions to the dashboard

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -4,9 +4,14 @@ const addTransaction = (req, res) => {
     const { description, amount, type } = req.body;
     const transaction = new Transaction(description, amount, type);
     transaction.save();
-
-
-    res.redirect('/');
+    Transaction.addTransaction(description, amount, type)
+        .then(() => {
+            res.redirect('/index');
+        })
+        .catch(error => {
+            console.error('Error al agregar la transacción:', error);
+            res.redirect('/index');
+        });
 };
 
 module.exports = {addTransaction};
